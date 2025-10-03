41c1baea8d7ded3fc76a6cf26b69b823522e3fe0
Omnipotara <ognjenpavlovic44@Gmail.com>
2025-10-03 19:06:04 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter, updateCounter] = useState(0);
+  var message;
+  if (counter < 8) {
+    message = <h1>{counter} / 8 cups of water drank today</h1>;
+  } else {
+    message = <h2>Bravo! You've reached your goal of 8 cups of water!</h2>;
+  }
+
+  return (
+    <div className="app-container">
+      <div
+        className="water-fill"
+        style={{ height: `${counter * 12.5}vh` }}
+      ></div>
+
+      <img
+        src="/pngtree-sticker-of-a-cartoon-water-bottle-png-image_14957724.png"
+        alt="water-bottle"
+        className="water-image"
+      />
+      {message}
+      <br />
+      {counter < 8 ? (
+        <button
+          className="water-button"
+          onClick={() => updateCounter(counter + 1)}
+        >
+          Drink a cup of water
+        </button>
+      ) : (
+        <button className="water-button" onClick={() => updateCounter(0)}>
+          Reset the counter
+        </button>
+      )}
+    </div>
+  );
+}
+export default App;
